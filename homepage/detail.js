b82fe9fe8f6c68d29e7c3a4511063c04668b92ee
Sarthak-Kar <sarthak.kar0501@gmail.com>
2022-02-17 18:20:06 -0800

    make it better

--- homepage/detail.js
+++ homepage/detail.js
@@ -1,15 +1,15 @@
 async function getdata() {
     const response = await fetch('/getdata');
     const temp = await response.json();
-    
+
     var index, i;
-    for ( i = 0; i < temp.results.rows.length; i++) {
-        if(temp.results.rows[i]['name']==queryString){
+    for (i = 0; i < temp.results.rows.length; i++) {
+        if (temp.results.rows[i]['name'] == queryString) {
             index = i;
             break;
         }
     }
-    var target = temp.results.rows[i];
+    const target = temp.results.rows[i];
     document.getElementById("uid").innerHTML = target['uid'];
     document.getElementById("name").innerHTML = target['name'];
     document.getElementById("width").innerHTML = target['width'];
@@ -18,12 +18,49 @@ async function getdata() {
     document.getElementById("opacity").innerHTML = target['opacity'];
 
     document.getElementById("box").style.backgroundColor = target['color'];
-    document.getElementById("box").style.opacity = target['opacity']/100;
-    var new_width = target['width']+100;
-    document.getElementById("box").style.width = new_width+"px";
-    var new_height = target['height']+100;
-    document.getElementById("box").style.height = new_height+"px";
-    document.getElementById("preview_name").innerHTML = target['name'];   
+    document.getElementById("box").style.opacity = target['opacity'] / 100;
+    var new_width = target['width'] + 100;
+    document.getElementById("box").style.width = new_width + "px";
+    var new_height = target['height'] + 100;
+    document.getElementById("box").style.height = new_height + "px";
+    document.getElementById("preview_name").innerHTML = target['name'];
+}
+
+async function update(str) {
+    const response = await fetch('/getdata');
+    const temp = await response.json();
+
+    var index, i;
+    for (i = 0; i < temp.results.rows.length; i++) {
+        if (temp.results.rows[i]['name'] == queryString) {
+            index = i;
+            break;
+        }
+    }
+    const target = temp.results.rows[i];
+
+    var val = document.getElementById(str).value;
+    if (val.length == 0) {
+        return;
+    }
+    var field = str.slice(1);
+    var str = { code: "update data set " + field + "='" + val + "' where " + field + "='" + target[field] + "';" };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(str),
+    };
+
+    var wait = await fetch('/update', options);
+    if(document.getElementById("aname").value.length == 0){
+        document.location.reload(true);
+    }
+    else{
+        location.href = "detail.html?" + document.getElementById("aname").value;    
+    }
+
 }
 
 function gotohomepage() {
